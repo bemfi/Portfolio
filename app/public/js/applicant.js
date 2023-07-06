824d0c7c196a83b002bee61642472ade166c7576
Benny <benjamin.fischer@stud.hs-kempten.de>
2023-07-06 22:27:09 +0200

    kleine änderungen

--- app/public/js/applicant.js
+++ app/public/js/applicant.js
@@ -25,66 +25,55 @@ fetch('/api/applicants')
       if (element) {
         if (key === 'bild') {
           element.style.backgroundImage = `url(${dataObject[key]})`;
+        } else if (key === 'bild2') {
+          element.src = `../${dataObject[key]}`;
         } else {
-          if (key === 'bild2') {
-            element.src = `../${dataObject[key]}`;
-          }
           element.textContent = dataObject[key];
         }
       }
     }
-  
 
     // Akademischer Werdegang abrufen
-  const applicantId = data[0].id;
-  fetch(`/api/applicants/${applicantId}/akademischerWerdegang`)
-    .then(response => response.json())
-    .then(akademischerWerdegang => {
+    const applicantId = data[0].id;
+    Promise.all([
+      fetch(`/api/applicants/${applicantId}/akademischerWerdegang`).then(response => response.json()),
+      fetch(`/api/applicants/${applicantId}/beruflicherWerdegang`).then(response => response.json()),
+      fetch(`/api/applicants/${applicantId}/faehigkeiten`).then(response => response.json())
+    ]).then(([akademischerWerdegang, beruflicherWerdegang, faehigkeiten]) => {
       // Akademischer Werdegang erfolgreich empfangen
       console.log(akademischerWerdegang);
+      console.log(beruflicherWerdegang);
+      console.log(faehigkeiten);
 
       // Daten in das entsprechende HTML-Element einfügen
       const akademischerWerdegangElement = document.querySelector('#akademischerWerdegang');
       akademischerWerdegangElement.innerHTML = ''; // Leeren des Inhalts des Elements
 
       akademischerWerdegang.forEach(eintrag => {
-        const abschluss = eintrag.abschluss;
-        const universitaet = eintrag.universitaet;
-        const zeitraum = eintrag.akadem_zeitraum;
-        const beschreibung = eintrag.akadem_beschreibung;
+        const { abschluss, universitaet, akadem_zeitraum, akadem_beschreibung } = eintrag;
 
         // Erstellen und Hinzufügen des HTML-Codes für einen Eintrag im akademischen Werdegang
         const eintragHTML = `
           <div class="eintrag">
             <h3>${abschluss}</h3>
             <p>${universitaet}</p>
-            <p>${zeitraum}</p>
-            <p>${beschreibung}</p>
+            <p>${akadem_zeitraum}</p>
+            <p>${akadem_beschreibung}</p>
           </div>
         `;
         akademischerWerdegangElement.innerHTML += eintragHTML;
       });
-    })
-    fetch(`/api/applicants/${applicantId}/beruflicherWerdegang`)
-    .then(response => response.json())
-    .then(beruflicherWerdegang => {
-      // Akademischer Werdegang erfolgreich empfangen
-      console.log(beruflicherWerdegang);
 
-      // Daten in das entsprechende HTML-Element einfügen
       const beruflicherWerdegangElement = document.querySelector('#beruflicherWerdegang');
-      beruflicherWerdegang.innerHTML = ''; // Leeren des Inhalts des Elements
+      beruflicherWerdegangElement.innerHTML = ''; // Leeren des Inhalts des Elements
 
       beruflicherWerdegang.forEach(eintrag => {
-        const position = eintrag.position;
-        const unternemen = eintrag.unternehmen;
-        const zeitraum = eintrag.zeitraum;
-        const beschreibung = eintrag.beschreibung;
+        const { position, unternehmen, zeitraum, beschreibung } = eintrag;
 
-        // Erstellen und Hinzufügen des HTML-Codes für einen Eintrag im akademischen Werdegang
+        // Erstellen und Hinzufügen des HTML-Codes für einen Eintrag im beruflichen Werdegang
         const eintragHTML = `
           <div class="eintrag">
-            <h3>${unternemen}</h3>
+            <h3>${unternehmen}</h3>
             <p>${position}</p>
             <p>${zeitraum}</p>
             <p>${beschreibung}</p>
@@ -92,95 +81,31 @@ fetch('/api/applicants')
         `;
         beruflicherWerdegangElement.innerHTML += eintragHTML;
       });
-    })
-    fetch(`/api/applicants/${applicantId}/faehigkeiten`)
-    .then(response => response.json())
-    .then(faehigkeiten => {
-      // Akademischer Werdegang erfolgreich empfangen
-      console.log(faehigkeiten);
 
-      // Daten in das entsprechende HTML-Element einfügen
       const faehigkeitenElement = document.querySelector('#faehigkeiten');
-      faehigkeiten.innerHTML = ''; // Leeren des Inhalts des Elements
+      faehigkeitenElement.innerHTML = ''; // Leeren des Inhalts des Elements
 
       faehigkeiten.forEach(eintrag => {
-        const faehigkeiten = eintrag.faehigkeit;
-        const koennen = eintrag.koennen;
-        
+        const { faehigkeit, koennen } = eintrag;
 
-        // Erstellen und Hinzufügen des HTML-Codes für einen Eintrag im akademischen Werdegang
+        // Erstellen und Hinzufügen des HTML-Codes für einen Eintrag in den Fähigkeiten
         const eintragHTML = `
           <div class="eintrag">
-            <p class="display">${faehigkeiten}</p>
+            <p class="display">${faehigkeit}</p>
             <p class="display">${koennen}</p>
           </div>
         `;
         faehigkeitenElement.innerHTML += eintragHTML;
       });
-    })
+    }).catch(error => console.error(error));
   })
   .catch(error => console.error(error));
-
-
 
 
   const API_URL = `https://api.thecatapi.com/v1/`;
   const API_KEY = "DEMO-API-KEY";
   
   let currentImageToVoteOn;
-  
-  function showHistoricVotes()
-  {
-    
-    document.getElementById('vote-options').style.display = 'none';
-    document.getElementById('vote-results').style.display = 'block';
-  
-    const url = `${API_URL}votes?limit=10&order=DESC`;
-  
-    fetch(url,{headers: {
-      'x-api-key': API_KEY
-    }})
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-    
-      data.map(function(voteData) {
-   
-      const imageData = voteData.image
-   
-      let image = document.createElement('img');
-       //use the url from the image object
-       image.src = imageData.url
-              
-      let gridCell = document.createElement('div');
-      
-        if(voteData.value<0)
-        {
-          gridCell.classList.add('red') 
-        } else {
-          gridCell.classList.add('green')
-        }
-        
-      gridCell.classList.add('col-lg');
-  
-      gridCell.appendChild(image)
-         
-      document.getElementById('grid').appendChild(gridCell);
-         
-      });
-    
-    })
-    .catch(function(error) {
-       console.log(error);
-    });
-    
-  }
-  
-  function showVoteOptions()
-  {
-    showImageToVoteOn()
-  }
   
   function showImageToVoteOn()
   {
@@ -213,8 +138,8 @@ fetch('/api/applicants')
       'x-api-key': API_KEY
     }})
     .then((response) => {
-      showVoteOptions()
+      showImageToVoteOn()
     })
   }
   
-  showVoteOptions()
+  showImageToVoteOn()
